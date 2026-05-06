9cedae70f926f37394d74123bb6203dc58b062d5
Esmael <82362802+esmael1997@users.noreply.github.com>
2026-05-06 20:36:16 +0330

    feat(accounts): implement reusable regex validation layer for customer data (phone, national_id, name)
    This update introduces a reusable validation layer inside the accounts app using Django's RegexValidator.
    
    The following validations were implemented:
    
    📱 Phone number validation in international format (+989xxxxxxxxx)
    🆔 National ID validation (10-digit numeric format)
    🧾 Name validation (Persian letters only)
    
    All validators are modularized under a dedicated validators/ package to ensure:
    
    Code reusability
    Separation of concerns
    Maintainability in large-scale backend systems
    
    A Customer model was also created and connected to these validators, with successful database migration and runtime validation tested via Django shell.
    
    📌 Key Improvements
    Introduced structured validation architecture (validators/ module)
    Enforced data integrity at model level
    Verified functionality using Django ORM and shell testing
    Prepared foundation for future authentication and API layers
    📌 Tech Stack Used
    Django
    Docker (containerized environment)
    Django ORM
    RegexValidator (Django core validators)

--- core/accounts/models.py
+++ core/accounts/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+from accounts.validators import phone_validator,national_id_validator,name_validator
+
+class Customer(models.Model):
+    name= models.CharField(max_length=150, validators=[name_validator])
+    phone = models.CharField(max_length=13, validators=[phone_validator])
+    national_id = models.CharField(max_length=10, validators=[national_id_validator]) 
