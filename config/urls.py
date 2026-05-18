7253b5c1b6680d38096e54484438a46399ebf6e4
Esmael <82362802+esmael1997@users.noreply.github.com>
2026-05-18 20:07:50 +0330

    Implement shop models, admin panel, and product list with category filtering

--- config/urls.py
+++ config/urls.py
@@ -21,4 +21,5 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('apps.accounts.urls')),
+    path('',include('shop.urls')),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product, Category
 
-# Create your views here.
+def product_list(request, slug=None):
+    category = None
+    products = Product.objects.filter(available=True)
+    if slug:
+        category = get_object_or_404(Category, slug=slug)
+        products = products.filter(category=category)
+    context = {"products": products}
+    return render(request, "shop/product_list.html", context)
