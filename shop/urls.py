65aa04e2c6df9d40d25230b3d8bf3326bc59a08e
Esmael <82362802+esmael1997@users.noreply.github.com>
2026-05-19 20:23:42 +0330

    Implement product detail page with SEO-friendly routing

--- shop/urls.py
+++ shop/urls.py
@@ -6,4 +6,5 @@ app_name = "shop"
 urlpatterns = [
     path("", views.product_list, name="product_list"),
     path("category/<slug:slug>/",views.product_list,name="product_list_by_category",),
+    path("<int:id>/<slug:slug>/", views.product_detail, name="product_detail"),
 ]
--- shop/views.py
+++ shop/views.py
@@ -7,5 +7,10 @@ def product_list(request, slug=None):
     if slug:
         category = get_object_or_404(Category, slug=slug)
         products = products.filter(category=category)
-    context = {"products": products}
+    context = {"category":category,"products": products,}
     return render(request, "shop/product_list.html", context)
+
+def product_detail(request, id, slug):
+    product = get_object_or_404(Product, id=id, slug=slug, available=True,)
+    context = {"product": product,}
+    return render(request, "shop/product_detail.html", context,)
