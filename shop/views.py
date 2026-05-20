59021c15df9b5cc4f504a51c849d7925411deb71
Esmael <82362802+esmael1997@users.noreply.github.com>
2026-05-20 18:32:09 +0330

    Add pagination to product listing view

--- shop/views.py
+++ shop/views.py
@@ -1,16 +1,33 @@
+from urllib import request
+from django.core import paginator
 from django.shortcuts import render, get_object_or_404
 from .models import Product, Category
+from django.core.paginator import Paginator
 
 def product_list(request, slug=None):
     category = None
     products = Product.objects.filter(available=True)
+
+    paginator = Paginator(products, 3)
+
+    page_number = request.GET.get("page")
+
+    page_obj = paginator.get_page(page_number)
+
+
     if slug:
         category = get_object_or_404(Category, slug=slug)
         products = products.filter(category=category)
-    context = {"category":category,"products": products,}
+    context = {"category":category,"products": page_obj,"page_obj": page_obj,}
     return render(request, "shop/product_list.html", context)
 
 def product_detail(request, id, slug):
-    product = get_object_or_404(Product, id=id, slug=slug, available=True,)
-    context = {"product": product,}
+    products = Product.objects.filter(available=True)
+
+    paginator = Paginator(products, 3)
+
+    page_number = request.GET.get("page")
+
+    page_obj = paginator.get_page(page_number)
+    context = {"products": page_obj,"page_obj": page_obj,}
     return render(request, "shop/product_detail.html", context,)
